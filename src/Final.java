219ce2a349639a640917aadeef917fc3baa7e541
abentler <abentler@calpoly.edu>
2024-05-17 16:20:50 -0700

    Improved and verified parsing

--- src/Final.java
+++ src/Final.java
@@ -1,18 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Final {
 
     public static int NUM_FEATURES = 58;
-    static double[][] data = new double[4601][NUM_FEATURES];
+    static ArrayList<double[]> data = new ArrayList<>();
 
     public static void main(String[] args) {
         File file;
         // getting file path
         if(args.length < 1) {
             // our database is probably in current directory
-            System.out.println("Using default path ('./spambase.data' for dataset.");
+            System.out.println("Using default path ('./spambase.data') for dataset.");
             file = new File("spambase.data");
         } else {
             // but you can also put in the path as an arg
@@ -36,7 +37,7 @@ public class Final {
             for(int i = 0; i < NUM_FEATURES; i++) {
                 values[i] = Double.parseDouble(line[i]);
             }
-            data[rowCount++] = values;
+            data.add(values);
         }
     }
 }
