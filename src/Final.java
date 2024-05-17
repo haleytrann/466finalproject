0dfd76501f789eed7a5de89d3a71b77e770f42a4
abentler <abentler@calpoly.edu>
2024-05-17 11:56:54 -0700

    Framework for parsing

--- src/Final.java
+++ src/Final.java
@@ -1,6 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Final {
 
+    int data[][];
+
     public static void main(String[] args) {
+        if(args.length < 1) {
+            System.err.println("Requires data filepath as argument.");
+            return;
+        }
+        File file = new File(args[0]);
+        try {
+            parseData(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void parseData(File file) throws FileNotFoundException {
+        Scanner input = new Scanner(file);
         
     }
 }
