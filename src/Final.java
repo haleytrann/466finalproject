c9a0bf8ae70fc0c0462f6b2b246422bf498c3336
abentler <abentler@calpoly.edu>
2024-05-17 12:05:58 -0700

    First draft of parsing

--- src/Final.java
+++ src/Final.java
@@ -4,23 +4,39 @@ import java.util.Scanner;
 
 public class Final {
 
-    int data[][];
+    public static int NUM_FEATURES = 58;
+    static double[][] data = new double[4601][NUM_FEATURES];
 
     public static void main(String[] args) {
+        File file;
+        // getting file path
         if(args.length < 1) {
-            System.err.println("Requires data filepath as argument.");
-            return;
+            // our database is probably in current directory
+            System.out.println("Using default path ('./spambase.data' for dataset.");
+            file = new File("spambase.data");
+        } else {
+            // but you can also put in the path as an arg
+            file = new File(args[0]);
         }
-        File file = new File(args[0]);
+
         try {
-            parseData(file);
-        } catch (FileNotFoundException e) {
+            parseData(file); // parse data into arrays
+
+        } catch (FileNotFoundException e) { // catch file not found exception
             e.printStackTrace();
         }
     }
 
     public static void parseData(File file) throws FileNotFoundException {
         Scanner input = new Scanner(file);
-        
+        int rowCount = 0;
+        while(input.hasNextLine()) {
+            String[] line = input.nextLine().split(",");
+            double values[] = new double[NUM_FEATURES];
+            for(int i = 0; i < NUM_FEATURES; i++) {
+                values[i] = Double.parseDouble(line[i]);
+            }
+            data[rowCount++] = values;
+        }
     }
 }
