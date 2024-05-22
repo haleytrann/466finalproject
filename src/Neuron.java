ff54908e2fb442d2e0d70a2ba958f95c19a566b4
zarinayunis <zyunis@calpoly.edu>
2024-05-22 11:57:46 -0700

    committing neuron changes

--- src/Neuron.java
+++ src/Neuron.java
@@ -4,11 +4,16 @@ public class Neuron {
     Random random = new Random();
     public double bias = random.nextDouble(-1, 1);
     public double [] weights = new double[58];
+    public Neuron() {
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = random.nextDouble(-1, 1);
+        }
+    }
     public double compute(double [] inputs) {
         double initOutput = 0;
         for (int i = 0; i < inputs.length; i++) {
             initOutput += (inputs[i] * weights[i]);
         }
-        return initOutput;
+        return (1/(1 + Math.exp(-(initOutput + bias))));
     }
 }
