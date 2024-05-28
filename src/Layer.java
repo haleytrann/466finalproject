df78af8e544a39c441655a633cbb0d0f6a765d48
bchan42 <bchan3k@gmail.com>
2024-05-28 11:33:28 -0700

    create Layer class

--- src/Layer.java
+++ src/Layer.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Layer {
+import java.util.ArrayList;
+
+public class Layer {
+
+    private ArrayList<Neuron> networkLayer = new ArrayList<>();
+
+    public Layer(int numberOfNeurons){
+        for (int i = 0; i < numberOfNeurons; i++) {
+            networkLayer.add(new Neuron());
+        }
+    }
+
+    public Neuron getNeuron(int index){
+        if(index <= networkLayer.size() && index >= 0){
+            return networkLayer.get(index);
+        }
+        else{
+            return null;
+        }
+    }
+
+    public int getNumberOfNeurons(){
+        return networkLayer.size();
+    }
+
 }
